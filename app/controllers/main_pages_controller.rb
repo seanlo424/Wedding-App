2b81768be4946893a4f4278a9e24c41c1da7f947
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2020-02-17 01:02:53 +0000

    Fixed my comment in controller file

--- app/controllers/main_pages_controller.rb
+++ app/controllers/main_pages_controller.rb
@@ -3,4 +3,4 @@ class MainPagesController < ApplicationController
 	def index
 	end
 	# By adding this index allows us to connect this controller to the index.html.erb file as the landing page.
-endng 
+end
