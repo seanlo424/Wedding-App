c021c0a96bf0074118cb4e5fa16e47f416f49c1b
Sean O'Neil <sean.oneil424@gmail.comgit config --global user.name Sean>
2020-02-17 00:59:40 +0000

    Created the main_pages controller and Added the index page

---
+++ app/controllers/main_pages_controller.rb
@@ -0,0 +1,6 @@
+class MainPagesController < ApplicationController
+
+	def index
+	end
+	# By adding this index allows us to connect this controller to the index.html.erb file as the landing page.
+endng 
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main_pages#index'
+  #  This root hooks up the routes so my localhost:3030 will take me to the landing page.
 end
